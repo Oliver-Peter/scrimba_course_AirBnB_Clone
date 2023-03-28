4b6a70ba0adf3f6cab37bfd1206c928e03cdafca
OliPet <web.oliverpeter@gmail.com>
2023-03-28 12:21:05 +0200

    finish basic hmtl and styles for a card template

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,13 @@ export default function Card() {
       <div className='card'>
         <img className='card__img' src={CardLogo}></img>
         <div className='card__content'>
-          <img className='card__rating' src={Star}></img>
-          <p className='card__description'></p>
-          <p className='card__price'></p>
+          <div className='rating'> 
+            <img className='rating__symbol' src={Star}></img>
+            <p className='rating__text'>5.0<span>(6) / USA</span></p>
+          </div>
+          <p className='card__description'>Life lessons with Katie Zaferes</p>
+          <p className='card__price'><span>From $136</span> / person
+</p>
         </div>
       </div>
     </section>
