547cc224179e2089c99e55656caa9199a03cfa31
OliPet <web.oliverpeter@gmail.com>
2023-04-06 18:04:09 +0200

    add attributes and values to the Card instance

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import Hero from './components/Hero.js'
 import Card from './components/Card.js'
 
 
+
 export default function App () {
   return (
     <div className='container'>
       <Header />
       <div className='main-wrapper'>
         <Hero />
-        <Card />
+        <section className='cards'>
+          <Card 
+            img="katie-zaferes.png"
+            rating='5.0'
+            reviewCount={6}
+            country='USA'
+            title='Life lessons with Katie Zaferes'
+            price={136}
+          />
+        </section>
       </div>
     </div>
   )
