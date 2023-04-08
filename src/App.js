97852bb84fd91a9db2397a2ffda85251d9f235c0
OliPet <web.oliverpeter@gmail.com>
2023-04-08 12:13:35 +0200

    add 'database' for card Componentes and implement map method to return a Card Instanz for each data set / better class name for card section

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import Header from './components/Header.js'
 import Hero from './components/Hero.js'
 import Card from './components/Card.js'
+import cardData from './cardData.js'
 
 
 
 export default function App () {
+
+  const cards = cardData.map(data => {
+    return <Card 
+              key={data.id} 
+              img={data.coverImg} 
+              rating={data.stats.rating} 
+              reviewCount={data.stats.reviewCount} 
+              location={data.location} 
+              title={data.title} 
+              price={data.price}
+            />
+          })
+
   return (
     <div className='container'>
       <Header />
       <div className='main-wrapper'>
         <Hero />
-        <section className='cards'>
-          <Card 
-            img="katie-zaferes.png"
-            rating='5.0'
-            reviewCount={6}
-            country='USA'
-            title='Life lessons with Katie Zaferes'
-            price={136}
-          />
+        <section className='cards-list'>
+          {cards}
         </section>
       </div>
     </div>
