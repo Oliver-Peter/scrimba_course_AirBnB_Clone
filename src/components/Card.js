770e7d623f2b8cea0d2a867c40fa6d63b77f389a
OliPet <web.oliverpeter@gmail.com>
2023-04-08 12:15:53 +0200

    change import path for logo / change prop names

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Star from '../images/star.png';
+import Star from '/public/images/star.png';
 
 
 
@@ -9,11 +9,11 @@ export default function Card(props) {
   return (
     
     <div className='card'>
-      <img className='card__img' src={require(`../images/${props.img}`)}></img>
+      <img className='card__img' src={`../images/${props.img}`}></img>
       <div className='card__content'>
         <div className='rating'> 
           <img className='rating__symbol' src={Star}></img>
-          <p className='rating__text'>{props.rating}<span>{props.reviewCount} / {props.country}</span></p>
+          <p className='rating__text'>{props.rating}<span>({props.reviewCount}) / {props.location}</span></p>
         </div>
         <p className='card__description'>{props.title}</p>
         <span className='card__price'>From ${props.price}</span><span> / person</span>
