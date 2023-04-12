e723322d336d15a43db485f7f67260cc66a4f3f0
OliPet <web.oliverpeter@gmail.com>
2023-04-12 21:26:43 +0200

    add html for badge text and implement its logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,23 @@ import Star from '/public/images/star.png';
 
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOULD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  } else {
+    badgeText = `${props.openSpots} SPOTS`
+  }
   return (
     
     <div className='card'>
-      <img className='card__img' src={`../images/${props.img}`}></img>
+      {badgeText && <div className='card__badge'>{badgeText}</div> }
+      <img className='card__img' src={`../images/${props.coverImg}`}></img>
       <div className='card__content'>
         <div className='rating'> 
           <img className='rating__symbol' src={Star}></img>
-          <p className='rating__text'>{props.rating}<span>({props.reviewCount}) / {props.location}</span></p>
+          <p className='rating__text'>{props.stats.rating}<span>({props.stats.reviewCount}) / {props.location}</span></p>
         </div>
         <p className='card__description'>{props.title}</p>
         <span className='card__price'>From ${props.price}</span><span> / person</span>
